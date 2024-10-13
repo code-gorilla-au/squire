4b7b0104609792f0b06c7a43e0da413857a1c207
muz-ooh <mauricio.riquelme@oohmedia.com.au>
2024-10-13 23:20:03 +1100

    wip adding type

--- packages/fetcher/src/types.ts
+++ packages/fetcher/src/types.ts
@@ -3,11 +3,11 @@ export type Result<T> = Success<T> | Failure<T>;
 export type Failure<T> = {
 	data?: T | Record<string, unknown> | undefined;
 	error: Error;
-	statusCode: number;
+	status: number;
 	statusText: string;
 };
 
 export type Success<T> = {
 	error: never;
-	data: T;
+	data: T | Record<string, unknown> | null | undefined;
 };
