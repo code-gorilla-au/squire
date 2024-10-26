fe198ab4900e7c23b43cd333affdd3852b16dae8
muz-ooh <mauricio.riquelme@oohmedia.com.au>
2024-10-27 08:19:01 +1100

    wip

--- apps/client/src/routes/products/[id]/+page.svelte
+++ apps/client/src/routes/products/[id]/+page.svelte
@@ -1,10 +1,18 @@
 <script lang="ts">
+import Card from "$components/ui/card/card.svelte";
 import type { PageData } from "./$types";
 
 export let data: PageData;
 </script>
 
 <h1>Product: {data.props.product.name} </h1>
+
+<div>
+
+    <Card>
+
+    </Card>
+</div>
 
 <svelte:head>
     <title>Product | Squire</title>
