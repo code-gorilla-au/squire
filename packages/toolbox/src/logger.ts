285a86b0eb9f54918475bba4f9ddfc8e820f54d0
muz-ooh <mauricio.riquelme@oohmedia.com.au>
2024-10-26 21:29:58 +1100

    using import meta

--- packages/toolbox/src/logger.ts
+++ packages/toolbox/src/logger.ts
@@ -1,6 +1,6 @@
 import pino from "pino";
 import pinoCaller from "pino-caller";
 
-const base = pino({ level: process.env.LOG_LEVEL ?? "info" });
+const base = pino({ level: import.meta.env.LOG_LEVEL ?? "info" });
 
 export const logger = pinoCaller(base);
