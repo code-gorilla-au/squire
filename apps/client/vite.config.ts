06c8ca0acc53f73c6b26e6f4c3789580c94301fe
muz-ooh <mauricio.riquelme@oohmedia.com.au>
2024-10-26 21:29:31 +1100

    adding hack

--- apps/client/vite.config.ts
+++ apps/client/vite.config.ts
@@ -1,10 +1,17 @@
 import { defineConfig } from "vitest/config";
-import { sveltekit } from '@sveltejs/kit/vite';
+import { sveltekit } from "@sveltejs/kit/vite";
+import { type UserConfig, loadEnv } from "vite";
 
-export default defineConfig({
-    plugins: [sveltekit()],
+export default function config({ mode }: UserConfig) {
+	process.env = {
+		...process.env,
+		...loadEnv(mode ?? "development", "../../"),
+	};
+	return defineConfig({
+		plugins: [sveltekit()],
 
-    test: {
-        include: ['src/**/*.{test,spec}.{js,ts}']
-    }
-});
+		test: {
+			include: ["src/**/*.{test,spec}.{js,ts}"],
+		},
+	});
+}
