f1d04793c83de6588beedf54437346c010afbeda
muz-ooh <mauricio.riquelme@oohmedia.com.au>
2024-10-28 08:14:02 +1100

    adding open sec

--- apps/client/src/routes/+page.server.ts
+++ apps/client/src/routes/+page.server.ts
@@ -3,10 +3,12 @@ import type { PageServerLoad } from "./$types";
 
 export const load: PageServerLoad = async () => {
 	const products = await service.getAllProducts();
+	const securityAdvisories = await service.getAllSecurityAdvisories();
 
 	return {
 		props: {
 			products,
+			securityAdvisories,
 		},
 	};
 };
--- apps/client/src/routes/+page.svelte
+++ apps/client/src/routes/+page.svelte
@@ -3,7 +3,9 @@ import { goto } from "$app/navigation";
 import Tag from "$components/tag.svelte";
 import Button from "$components/ui/button/button.svelte";
 import Card from "$components/ui/card/card.svelte";
+import { format } from "date-fns";
 import type { PageData } from "./$types";
+import { cn } from "$lib/utils";
 
 export let data: PageData;
 
@@ -14,6 +16,19 @@ function routeToProducts() {
 function routeToProduct(id: string) {
 	goto(`/products/${id}`);
 }
+
+function styleTagBySeverity(severity: string) {
+	switch (severity) {
+		case "MODERATE":
+			return "bg-yellow-100 text-yellow-800";
+		case "HIGH":
+			return "bg-destructive text-text-destructive-foreground";
+		case "CRITICAL":
+			return "bg-destructive text-text-destructive-foreground";
+		default:
+			return "";
+	}
+}
 </script>
 
 <h1 class="heading-1">Dashboard</h1>
@@ -21,6 +36,26 @@ function routeToProduct(id: string) {
 <div>
     <a class="text-xs text-link" href="/products/create">Add additional products</a>
 </div>
+
+{#if data.props.securityAdvisories.length > 0}
+<div>
+    <h2 class="font-semibold my-4">Security Advisories</h2>
+    <div class="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4">
+        {#each data.props.securityAdvisories as advisory}
+            <a href={advisory.repoUrl}>
+                <Card class="p-3">
+                    <h3 class="font-semibold mb-3">{advisory.packageName}</h3>
+                    <p class="text-xs">Status: {advisory.state}</p>
+                    <p class="text-xs">Last Updated: {format(advisory.updatedAt, "yyyy-MM-dd")}</p>
+                    <Tag class="lowercase">{advisory.repoName}</Tag>
+                    <Tag class={cn("lowercase", styleTagBySeverity(advisory.severity))}>{advisory.severity}</Tag>
+                </Card>
+            </a>
+        {/each}
+    </div>
+</div>
+    
+{/if}
 
 {#if data.props.products.length > 0}
     <h2 class="font-semibold my-4">Products ({data.props.products.length})</h2>
