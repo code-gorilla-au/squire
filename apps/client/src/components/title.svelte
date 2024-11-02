71a70b6bb12e429f185eee044d4dd262d7e8585a
muz-ooh <mauricio.riquelme@oohmedia.com.au>
2024-11-02 18:15:40 +1100

    adding routing

---
+++ apps/client/src/components/title.svelte
@@ -0,0 +1,22 @@
+<script lang="ts">
+import { browser } from "$app/environment";
+import ChevronLeftIcon from "lucide-svelte/icons/chevron-left";
+import type { Snippet } from "svelte";
+
+let { children }: { children: Snippet } = $props();
+
+function goBack() {
+	if (!browser) {
+		return;
+	}
+
+	window.history.back();
+}
+</script>
+
+<div class="flex items-center">
+    <button onclick={goBack}>
+        <ChevronLeftIcon  />
+    </button>
+    <h1 class="heading-1">{@render children()}</h1>
+</div>
