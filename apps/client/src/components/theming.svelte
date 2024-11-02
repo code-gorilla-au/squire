ebd03d42b98b8e41d3892342b579fcb33a18b3c7
muz-ooh <mauricio.riquelme@oohmedia.com.au>
2024-11-02 15:39:37 +1100

    wip adding theme

---
+++ apps/client/src/components/theming.svelte
@@ -0,0 +1,32 @@
+<script lang="ts">
+import { Button } from "$components/ui/button";
+import { saveTheme, useTheme } from "$lib/theming";
+import Moon from "lucide-svelte/icons/moon";
+import Sun from "lucide-svelte/icons/sun";
+
+let { class: className }: { class?: string } = $props();
+
+let theme = useTheme();
+
+function toggleTheme() {
+	if (theme === "dark") {
+		saveTheme("light");
+		theme = "light";
+		return;
+	}
+
+	theme = "dark";
+	saveTheme("dark");
+}
+</script>
+    
+    <Button class={className} on:click={toggleTheme} variant="outline" size="icon">
+      <Sun
+        class="h-[1.2rem] w-[1.2rem] rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0"
+      />
+      <Moon
+        class="absolute h-[1.2rem] w-[1.2rem] rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100"
+      />
+      <span class="sr-only">Toggle theme</span>
+    </Button>
+    
---
+++ apps/client/src/lib/theming.ts
@@ -0,0 +1,72 @@
+const THEME_STORAGE_KEY = "--theme";
+export type Theme = "dark" | "light";
+
+/**
+ * media query for dark mode
+ */
+const darkMediaQuery = "(prefers-color-scheme: dark)";
+
+/**
+ * in memory store for the current theme
+ */
+let currentTheme: Theme | undefined = undefined;
+
+/**
+ * gets the current theme for the user based on their preference or saved theme
+ */
+export function useTheme(): Theme {
+	if (currentTheme) {
+		return currentTheme;
+	}
+
+	const prefersDark = window.matchMedia(darkMediaQuery).matches;
+
+	const storedTheme = localStorage.getItem(THEME_STORAGE_KEY) ?? "";
+	if (typeTheme(storedTheme)) {
+		currentTheme = storedTheme;
+		setColourScheme(storedTheme);
+		return currentTheme;
+	}
+
+	currentTheme = "light";
+	if (prefersDark) {
+		currentTheme = "dark";
+	}
+
+	setColourScheme(currentTheme);
+	return currentTheme;
+}
+
+/**
+ * saves the theme to local storage
+ * @param theme theme to save
+ */
+export function saveTheme(theme: Theme): void {
+	currentTheme = theme;
+	setColourScheme(theme);
+	localStorage.setItem(THEME_STORAGE_KEY, theme);
+}
+
+/**
+ * checks if value is a possible theme value
+ * @param value possible theme value
+ */
+function typeTheme(value: string): value is Theme {
+	return ["dark", "light"].includes(value);
+}
+
+function setColourScheme(theme: Theme) {
+	if (theme === "dark") {
+		document.documentElement.classList.add("dark");
+		return;
+	}
+
+	document.documentElement.classList.remove("dark");
+}
+
+/**
+ * resets the current theme
+ */
+export function resetTheme() {
+	currentTheme = undefined;
+}
