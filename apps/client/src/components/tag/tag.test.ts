cbe44245b37db5731a9d529e338cd0fdda12faf8
muz-ooh <mauricio.riquelme@oohmedia.com.au>
2024-11-02 21:02:14 +1100

    update

---
+++ apps/client/src/components/tag/tag.test.ts
@@ -0,0 +1,12 @@
+import { render, screen } from "@testing-library/svelte";
+import { describe, expect, it } from "vitest";
+
+import Tag from "./tag.test.svelte";
+
+describe("title component", () => {
+	it("should render the tag", () => {
+		render(Tag);
+
+		expect(screen.getByText("Test Tag")).toBeInTheDocument();
+	});
+});
