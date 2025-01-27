fbaf7f760b2da8929c8ae13c844e688602390648
muz-ooh <mauricio.riquelme@oohmedia.com.au>
2025-01-27 20:36:03 +1100

    wip fixing components

--- apps/client/src/components/ui/menubar/menubar-shortcut.svelte
+++ apps/client/src/components/ui/menubar/menubar-shortcut.svelte
@@ -1,6 +1,6 @@
 <script lang="ts">
-	import type { WithElementRef } from "bits-ui";
 	import type { HTMLAttributes } from "svelte/elements";
+	import type { WithElementRef } from "bits-ui";
 	import { cn } from "$lib/utils.js";
 
 	let {
@@ -8,13 +8,13 @@
 		class: className,
 		children,
 		...restProps
-	}: WithElementRef<HTMLAttributes<HTMLDivElement>> = $props();
+	}: WithElementRef<HTMLAttributes<HTMLSpanElement>> = $props();
 </script>
 
-<div
+<span
 	bind:this={ref}
-	class={cn("bg-card text-card-foreground rounded-lg border shadow-sm", className)}
+	class={cn("text-muted-foreground ml-auto text-xs tracking-widest", className)}
 	{...restProps}
 >
 	{@render children?.()}
-</div>
+</span>
