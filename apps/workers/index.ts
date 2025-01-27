1f6d49df84d165f87159a45ba913b7d218dbd26f
muz-ooh <mauricio.riquelme@oohmedia.com.au>
2025-01-27 12:28:05 +1100

    update

--- apps/workers/index.ts
+++ apps/workers/index.ts
@@ -4,21 +4,26 @@ import { logger } from "toolbox";
 import { loadConfig } from "./src/env";
 import { initDB } from "database";
 
+logger.debug("Starting worker");
 const config = loadConfig();
 
+logger.debug("Initializing DB");
 const db = await initDB(config.dbFilePath);
 
+logger.debug("Initializing client");
 const client = initClient({
 	ghToken: config.ghToken,
 	defaultOwner: config.ghOwner,
 });
 
+logger.debug("Initializing services");
 const repo = new ProductRepository(db, logger);
 const service = new ProductService(repo, logger, client);
 
+logger.info("Adding products");
 for (const topic of config.ghRepoTopics) {
 	try {
-		logger.info({ topic }, "Adding topic");
+		logger.info({ topic }, "Adding product");
 		await service.createProduct(topic, [topic]);
 	} catch (error) {
 		const err = error as Error;
