6ba9a4b6afe16eb02478723bd83030a0ea9c57c7
muz-ooh <mauricio.riquelme@oohmedia.com.au>
2025-02-09 08:03:49 +1100

    refactoring + adding test

--- apps/pre-ingest/index.ts
+++ apps/pre-ingest/index.ts
@@ -1,10 +1,10 @@
 import { initDB } from "database";
 import type { Level } from "pino";
 import { ProductRepository, ProductService } from "products";
-import { ProductExistsError } from "products/src/errors";
 import { initClient } from "squire-github";
 import { serverLogger } from "toolbox";
 import { loadConfig } from "./src/env";
+import { IngestService } from "./src/ingest";
 
 const config = loadConfig();
 const logger = serverLogger(config.VITE_LOG_LEVEL as Level);
@@ -24,37 +24,6 @@ await repo.initTables();
 
 logger.info("creating products...");
 
-const errList: Error[] = [];
-for (const topic of config.TOPICS_TO_INGEST) {
-	try {
-		logger.debug("Creating product for topic", topic);
-		await service.createProduct(topic, [topic]);
-	} catch (error) {
-		const err = error as Error;
-		if (err instanceof ProductExistsError) {
-			logger.warn({ topic }, "Product already exists, skipping...");
-			continue;
-		}
+const ingestService = new IngestService(logger, service);
 
-		logger.error({ error: err.message }, "Error creating product");
-		errList.push(err);
-	}
-}
-
-if (errList.length > 0) {
-	const totalErrs = JSON.stringify(errList);
-	logger.error({ totalErrs }, "Error creating products");
-
-	throw new Error("Error creating products");
-}
-
-logger.info("Products created, syncing...");
-const syncErrs = await service.syncProducts();
-if (syncErrs.length > 0) {
-	const totalErrs = JSON.stringify(syncErrs);
-	logger.error({ totalErrs }, "Error syncing products");
-
-	throw new Error("Error syncing products");
-}
-
-await db.close();
+await ingestService.ingest(config.TOPICS_TO_INGEST);
